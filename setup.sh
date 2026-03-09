0afed112aed5eef5432f50fa532d062eaea575dd
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-09 20:16:50 +0100

    Add project setup script and pyproject configuration

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+VENV=".venv"
+
+if [ ! -d "$VENV" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv $VENV
+
+    echo "Installing development dependencies..."
+    $VENV/bin/pip install --upgrade pip
+    $VENV/bin/pip install -e ".[dev]"
+else
+    echo "Virtual environment already exists."
+fi
+
+echo "Activating environment..."
+source $VENV/bin/activate
