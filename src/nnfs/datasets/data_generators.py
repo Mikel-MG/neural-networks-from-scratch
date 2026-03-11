106df81c52c646cdcdce5d55e771ab0a86c3324e
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-11 18:32:24 +0100

    Add data generator for two moons

--- src/nnfs/datasets/data_generators.py
+++ src/nnfs/datasets/data_generators.py
@@ -26,6 +26,46 @@ def generate_linear_data(
     X = np.linspace(xmin, xmax, n_samples)
     noise = np.random.normal(scale=noise_scale, size=n_samples)
     Y = X * m + b + noise
+
+    # reshape data
+    X = X.reshape(-1, 1)
+    Y = Y.reshape(-1, 1)
+
+    return (X, Y)
+
+
+def generate_two_moons(
+    n_samples: int,
+) -> tuple[np.ndarray, np.ndarray]:
+    """Generates x and y coordinates that represent two partially complementary half-moons.
+
+    Args:
+        n_samples (int): Number of data points to generate.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: X and Y data.
+    """
+    theta = np.random.rand(n_samples) * np.pi
+
+    #  generate data for the first moon
+    x1 = np.cos(theta)
+    y1 = np.sin(theta)
+
+    #  generate data for the second moon
+    x2 = 1 - x1
+    y2 = -y1 + 0.5
+
+    # stack data
+    X = np.vstack([np.stack([x1, y1], axis=1), np.stack([x2, y2], axis=1)])
+    Y = np.hstack([np.zeros(n_samples), np.ones(n_samples)])
+
+    # add noise
+    noise = np.random.rand(X.shape[0], 2)
+    X += noise
+
+    # scale vertical dimension
+    X[:, 1] *= 2
+
     # reshape data
     X = X.reshape(-1, 1)
     Y = Y.reshape(-1, 1)
