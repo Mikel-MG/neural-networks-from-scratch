ab232f00930e87412a47b29ee63b5c90ab2c98aa
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-12 21:22:02 +0100

    Improve documentation of layers module

--- src/nnfs/layers.py
+++ src/nnfs/layers.py
@@ -4,11 +4,10 @@ import numpy as np
 
 
 class BaseLayer(ABC):
-    """
-    Base class of a layer
+    """Template for neural network layers.
 
-    Layers may optionally define a `trainable` property returning
-    a list of (param, grad) tuples. Layers without trainable parameters
+    Layers may optionally define a `trainable` property that returns a
+    list of (param, grad) tuples. Layers without trainable parameters
     do not need to implement it.
     """
 
@@ -26,6 +25,22 @@ class BaseLayer(ABC):
 
 
 class Dense(BaseLayer):
+    """
+    A fully connected neural network layer.
+
+    Args:
+        input_size (int): Dimensionality of the input to the layer.
+        output_size (int): Dimensionality of the output of the layer.
+
+    Attributes:
+        W (np.ndarray): weight parameters.
+        b (np.ndarray): bias parameters.
+        dW (np.ndarray): gradient of loss w.r.t weight parameters.
+        db (np.ndarray): gradient of loss w.r.t bias parameters.
+        X_input (np.ndarray): cached input to the layer.
+
+    """
+
     def __init__(self, input_size: int, output_size: int):
         # layer parameters
         self.W = np.random.uniform(size=(input_size, output_size))
@@ -37,22 +52,29 @@ class Dense(BaseLayer):
         self.X_input = np.zeros(1)
 
     def forward(self, X_input: np.ndarray) -> np.ndarray:
-        """
-        Computes the forward pass for this layer
+        """Computes the forward pass for the layer.
 
-        For example, for the first layer:
-            * X_input: M_samples x N_features
-            * self.W: N_features x output_size
-            * self.b: 1 x output_size
-            * output: M_samples x output_size
+        Args:
+            X_input (np.ndarray): Input data to be transformed by the layer.
+
+        Returns:
+            np.ndarray: Output of the layer.
+
         """
         self.X_input = X_input
         output = np.matmul(X_input, self.W) + self.b
         return output
 
-    def backward(self, grad_next: np.ndarray):
-        """
-        Computes the backward pass for this layer
+    def backward(self, grad_next: np.ndarray) -> np.ndarray:
+        """Computes the backward pass for the layer.
+
+        This function updates the `dW` and `db` attributes of the layer.
+
+        Args:
+            grad_next (np.ndarray): Gradients fed back from the next layer during backpropagation.
+
+        Returns:
+            np.ndarray: Gradient of the loss w.r.t the input to the layer.
         """
         # Gradients w.r.t parameters
         self.dW = np.matmul(self.X_input.T, grad_next)  # shape: (input_dim, output_dim)
@@ -64,10 +86,12 @@ class Dense(BaseLayer):
 
     @property
     def trainable(self) -> list[tuple[np.ndarray, np.ndarray]]:
-        """
-        Returns the layer's trainable parameters and their gradients.
+        """Returns the layer's trainable parameters and their gradients.
 
         Each element is a tuple (param, grad) representing a parameter array
-        and its corresponding gradient, which are usedfor updates by an optimizer.
+        and its corresponding gradient, which are used by the optimizer during training.
+
+        Returns:
+            list[tuple[np.ndarray, np.ndarray]]: A list of tuples containing `(parameter, grad_parameters)` for each trainable parameter.
         """
         return [(self.W, self.dW), (self.b, self.db)]
