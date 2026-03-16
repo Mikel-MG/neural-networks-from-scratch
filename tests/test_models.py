e198e05b95c9a17befa77ab885e8c26713ac53a0
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-16 23:38:43 +0100

    Add linear model optimization test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from nnfs.datasets.data_generators import generate_linear_data
 from nnfs.layers import Dense
 from nnfs.losses import MSE
@@ -26,4 +28,29 @@ def test_linear_model_training_step():
     y_pred = model.forward(X_data)
     loss_end = model.loss.forward(y_pred, y_true)
 
+    # check that loss value has decreased
     assert loss_end < loss_start
+
+
+def test_linear_model_training_optimization():
+    # generate data
+    X_data, y_true = generate_linear_data(3, -7, 5000)
+
+    # define the model
+    list_layers = [Dense(1, 1)]
+    loss_func = MSE()
+    optimizer = SGD(lr=0.01)
+    model = Sequential(list_layers, loss_func, optimizer)
+
+    # train the model
+    model.fit(X_data, y_true, N_epochs=5000)
+
+    # extract layer parameters
+    layer = model.layers[0]
+    assert isinstance(layer, Dense)
+    opt_W = layer.W.item()
+    opt_b = layer.b.item()
+
+    # check that optimized parameters match ideal values
+    assert 3 == pytest.approx(opt_W, abs=0.1)
+    assert -7 == pytest.approx(-7, opt_b, abs=0.1)
