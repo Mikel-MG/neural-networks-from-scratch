082d862799f00d6aa641f91c875f6c479a15935b
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-17 13:39:38 +0100

    Add nonlinear model training step test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,9 @@
 import pytest
 
-from nnfs.datasets.data_generators import generate_linear_data
+from nnfs.activations import Sigmoid
+from nnfs.datasets.data_generators import generate_linear_data, generate_XOR_gate
 from nnfs.layers import Dense
-from nnfs.losses import MSE
+from nnfs.losses import BCE, MSE
 from nnfs.model import Sequential
 from nnfs.optimizers import SGD
 
@@ -54,3 +55,28 @@ def test_linear_model_training_optimization():
     # check that optimized parameters match ideal values
     assert 3 == pytest.approx(opt_W, abs=0.1)
     assert -7 == pytest.approx(-7, opt_b, abs=0.1)
+
+
+def test_nonlinear_model_training_step():
+    # generate data
+    X_data, y_true = generate_XOR_gate()
+
+    # define the model
+    list_layers = [Dense(2, 2), Sigmoid(), Dense(2, 1), Sigmoid()]
+    loss_func = BCE()
+    optimizer = SGD()
+    model = Sequential(list_layers, loss_func, optimizer)
+
+    # compute initial loss
+    y_pred = model.forward(X_data)
+    loss_start = model.loss.forward(y_pred, y_true)
+
+    # run a training step
+    model.run_training_epoch(X_data, y_true)
+
+    # compute loss after training step
+    y_pred = model.forward(X_data)
+    loss_end = model.loss.forward(y_pred, y_true)
+
+    # check that loss value has decreased
+    assert loss_end < loss_start
