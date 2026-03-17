20610f695bd0286b0edf679f8c612336c300327a
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-17 14:16:27 +0100

    Add binary concentric circles data generation test

--- tests/test_data_generators.py
+++ tests/test_data_generators.py
@@ -1,7 +1,9 @@
 import pytest
 
-from nnfs.datasets.data_generators import (generate_concentric_circles,
-                                           generate_two_moons)
+from nnfs.datasets.data_generators import (
+    generate_concentric_circles,
+    generate_two_moons,
+)
 
 
 def test_two_moons_generator():
@@ -12,3 +14,15 @@ def test_two_moons_generator():
     # and that X and Y are two-dimensional arrays
     assert X.shape == pytest.approx([1000, 2])
     assert Y.shape == pytest.approx([1000, 1])
+
+
+def test_concentric_circles_binary():
+    # generate data
+    X, Y = generate_concentric_circles(500, 3, binary=True)
+
+    # check that the number of data points is as expected
+    assert X.shape == pytest.approx([1500, 2])
+    assert Y.shape == pytest.approx([1500, 1])
+
+    # check that the number of labels is as expected
+    assert len(set(Y.flatten())) == 2
