8f865edf001b67fd81c340b59138a314cbee829c
Mikel-MG <caballeromartinezgoiko@gmail.com>
2026-03-23 21:21:00 +0100

    Add momentum to SGD optimizer

--- src/nnfs/optimizers.py
+++ src/nnfs/optimizers.py
@@ -18,14 +18,20 @@ class SGD(BaseOptimizer):
     """Stochastic Gradient Descent optimizer.
 
     It implements the following update mechanism:
-    new_params = old_params - lr * gradients
+    new_params = old_params - lr * (momentum * velocity + gradients).
 
     Args:
-        lr (float): Learning rate.
+        lr (float): Learning rate, controls size of parameter update.
+        momentum (float): Ratio of previous parameter change that is retained.
+
+    Attributes:
+        v (dict): Stores velocity of gradient change as `dict[param_id] = np.ndarray`.
     """
 
-    def __init__(self, lr: float = 0.01):
+    def __init__(self, lr: float = 0.01, momentum: float = 0.0):
         self.lr = lr
+        self.momentum = momentum
+        self.v: dict = {}
 
     def update(
         self, param_id: str, parameters: np.ndarray, gradients: np.ndarray
@@ -37,5 +43,12 @@ class SGD(BaseOptimizer):
             parameters (np.ndarray): Model parameters.
             gradients (np.ndarray): dL / d_params gradients.
         """
+        # initialize gradient velocity array
+        if param_id not in self.v:
+            self.v[param_id] = np.zeros_like(gradients)
+
+        # update gradient velocity array
+        self.v[param_id] = self.momentum * self.v[param_id] + gradients
+
         # parameters are updated in-place
-        parameters -= self.lr * gradients
+        parameters -= self.lr * self.v[param_id]
